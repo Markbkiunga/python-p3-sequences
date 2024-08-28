787465de94e1c0741b861655e353a8c99e76a0bc
Mark Brian <markbkiunga2017@gmail.com>
2024-08-28 21:04:36 +0300

    Completes Lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,19 @@
 
 def print_fibonacci(length):
     pass
+    numbers_list = []
+    # print(dir(numbers_list))
+    if length == 0:
+        print(numbers_list)
+        return
+    if length >= 1:
+        numbers_list.append(0)
+    if length >= 2:
+        numbers_list.append(1)
+    for i in range(2, length):
+        next_value = numbers_list[-1] + numbers_list[-2]
+        numbers_list.append(next_value)
+    print(numbers_list)
+
+
+print_fibonacci(9)
